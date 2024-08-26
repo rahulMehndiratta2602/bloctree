f6b9f449bb9a825da714f853e298e2ed0951f329
rahulMehndiratta2602 <mehndirattarahul35@gmail.com>
2024-08-27 05:06:46 +0530

    testing rename

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,27 +2,29 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 
+// console.log("Nothing");
+
 dotenv.config({ path: './env' });
 
 //import transactionRouter from './routes/transaction.route.js'
-import accrouter from './routes/account.route.js'
-const app = express(); 
+import accrouter from './routes/account.route.js';
+const app = express();
 
- app.use(cors({
+app.use(cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true
-}))
+}));
 
 //app.use("/getTransaction" , transactionRouter)
-app.use("/account", accrouter)
+app.use("/account", accrouter);
 
- app.get('/', function (req, res) {  
+app.get('/', function (req, res) {
     res.json({
-        "name" : "dishant",
-    }); 
-});  
+        "name": "dishant",
+    });
+});
 
 
 app.listen(3000, () =>
     console.log('Example app listening on port 3000!'),
-  );
+);
